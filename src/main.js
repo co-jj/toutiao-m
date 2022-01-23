ffad9ba312a64b2a604777992bd08a779e7287a8
co-jj <244935844@qq.com>
2022-01-23 20:48:55 +0800

    项目初始化-引入vant组件库

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载全局样式
+import '@/styles/index.less'
+// 加载Van 核心组件
+import Vant from 'vant'
+// 加载Vant 全局样式
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
